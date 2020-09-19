516ae446720a425d775cb157151211a7b22c1626
numabesachi <numabesachi@gmail.com>
2020-09-20 01:49:39 +0900

    finish

---
+++ app/src/main/java/com/example/l4s_skill_countdown/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.l4s_skill_countdown
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var second = 10
+
+    val timer: CountDownTimer = object : CountDownTimer(10000, 1000) {
+        override fun onFinish() {
+            //startButton.isVisible = true
+            second = 10
+            secondText.text = second.toString()
+        }
+
+        override fun onTick(millisUntilFinised: Long) {
+            second -= 1
+            secondText.text = second.toString()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        secondText.text = second.toString()
+
+        startButton.setOnClickListener {
+            //startButton.isVisible = false
+            startButton.isClickable = false
+
+            timer.start()
+        }
+
+    }
+}
